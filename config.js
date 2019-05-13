2c3278d763e68cdd98358c94db0a9682d025cf1c
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-13 20:39:17 +0530

    removed extra config and fix pathPrefix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-aerial`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
   // social
 
   authorName: 'Anubhav Srivastava',
